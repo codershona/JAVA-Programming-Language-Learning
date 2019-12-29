961792c29489e57908a486e205a7a5fc95630971
codershona <falgunimst95@gmail.com>
2019-12-29 17:45:41 +0100

    java comments

---
+++ Java Theory/Java Comments.java
@@ -0,0 +1,19 @@
+// Java Comments
+// Comments can be used to explain Java code, and to make it more readable. It can also be used to prevent execution when testing alternative code.
+
+// Single-line comments start with two forward slashes (//).
+
+// Any text between // and the end of the line is ignored by Java (will not be executed).
+
+// This example uses a single-line comment before a line of code:
+
+// Example
+// // This is a comment
+// System.out.println("Hello World");
+
+public class MyClass {
+    public static void main(String[] args) {
+      // This is a comment
+      System.out.println("Hello World");
+    }
+  }
