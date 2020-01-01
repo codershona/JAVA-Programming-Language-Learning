c4dcc1a45ea90ba50e4e3ae6ebb695bf744291e5
codershona <falgunimst95@gmail.com>
2020-01-01 21:48:10 +0100

    data types, type casting and operators

---
+++ JAVA OPERATORS/Arithmetic Operators.java
@@ -0,0 +1,13 @@
+// Arithmetic Operators
+// Arithmetic operators are used to perform common mathematical operations.
+
+// Operator	Name	Description	Example	Try it
+// +	Addition	Adds together two values	x + y	
+// -	Subtraction	Subtracts one value from another	x - y	
+// *	Multiplication	Multiplies two values	x * y	
+// /	Division	Divides one value from another	x / y	
+// %	Modulus	Returns the division remainder	x % y	
+// ++	Increment	Increases the value of a variable by 1	++x	
+// --	Decrement	Decreases the value of a variable by 1	--x
+
+
