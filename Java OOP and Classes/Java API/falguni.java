5ce5ef6a70f2979a15418757fbc8f047c4e33b00
codershona <falgunimst95@gmail.com>
2020-01-05 18:40:56 +0100

    java api problemsolved in command prompt

---
+++ Java OOP and Classes/Java API/falguni.java
@@ -0,0 +1,13 @@
+package java.util;
+public class falguni
+{
+ 
+  public void show()
+  {
+     System.out.println("Allizwell");
+   }
+ public void view()
+  {
+     System.out.println("How are you?");
+   }
+}
